240479708f714e49978f2fd917115c591d92be50
Lisuzer <vovan20027206@gmail.com>
2022-05-25 23:40:16 +0300

    added users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeapotsModule } from './teapots/teapots.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TeapotsModule],
+  imports: [TeapotsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
