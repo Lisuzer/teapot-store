d4c4e5d2c528de3203b58adbe2b8925a977a5421
Lisuzer <vovan20027206@gmail.com>
2022-05-25 23:37:18 +0300

    added teapots

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeapotsModule } from './teapots/teapots.module';
 
 @Module({
-  imports: [],
+  imports: [TeapotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
