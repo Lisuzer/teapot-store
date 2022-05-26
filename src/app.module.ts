bf9c2c18471235117be4b792ca870ef81941709a
Lisuzer <vovan20027206@gmail.com>
2022-05-26 12:53:50 +0300

    added postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeapotsModule } from './teapots/teapots.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TeapotsModule, UsersModule],
+  imports: [
+    TeapotsModule,
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'teapot-store',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
