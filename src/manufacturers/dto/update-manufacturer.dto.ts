624c24a57b43903a83189bf2696a2ad02978ac99
Lisuzer <vovan20027206@gmail.com>
2022-05-31 12:53:39 +0300

    added manufacturers

--- src/manufacturers/dto/update-manufacturer.dto.ts
+++ src/manufacturers/dto/update-manufacturer.dto.ts
@@ -1,12 +1,21 @@
-import { OmitType } from "@nestjs/swagger";
-import { IsOptional } from "class-validator";
-import { ManufacturerDto } from "./manufacturer.dto";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 
-export class UpdateManufacturersDto extends OmitType(ManufacturerDto, ["name", "country"] as const) {
-    @IsOptional()
+export class UpdateManufacturersDto{
+    @IsString()
+    @ApiProperty({
+        description: "Manufacturer name",
+        required: false,
+        default: "GoodTea"
+    })
     name?: string;
 
-    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        description: "What country is the manufacturer from",
+        required: false,
+        default: "Ukrain"
+    })
     country?: string;
 }
