eae6b8bb394b76c4867787ebff761adb6d4b2fb4
Lisuzer <vovan20027206@gmail.com>
2022-07-19 00:16:04 +0300

    post on host

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: "http://localhost:8080",
     allowedHeaders: "*",
     credentials: true,
     preflightContinue: true
